0afa5108ef90c6c74dd1b0a4f8bcdf6c49752768
dong <904885508@qq.com>
2024-01-02 14:39:24 +0800

    feat(RedisSessionDao): alternative  EnterpriseCacheSessionDAO to CachingSessionDAO

--- src/main/java/com/hardcore/accounting/shiro/RedisSessionDao.java
+++ src/main/java/com/hardcore/accounting/shiro/RedisSessionDao.java
@@ -1,30 +1,40 @@
 package com.hardcore.accounting.shiro;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.cache.AbstractCacheManager;
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+import org.apache.shiro.cache.MapCache;
 import org.apache.shiro.session.Session;
-import org.apache.shiro.session.mgt.eis.EnterpriseCacheSessionDAO;
+import org.apache.shiro.session.mgt.eis.CachingSessionDAO;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 @Repository
 @Slf4j
-public class RedisSessionDao extends EnterpriseCacheSessionDAO {
+public class RedisSessionDao extends CachingSessionDAO {
     private static final long TIMEOUT = 30;
     private static final String SESSION_KEY_FORMAT = "xxx_session_%s";
     private final RedisTemplate<String, Object> sessionRedisTemplate;
 
     public RedisSessionDao(@Qualifier("sessionRedisTemplate") RedisTemplate<String, Object> sessionRedisTemplate) {
         this.sessionRedisTemplate = sessionRedisTemplate;
+        setCacheManager(new AbstractCacheManager() {
+            @Override
+            protected Cache<Serializable, Session> createCache(String name) throws CacheException {
+                return new MapCache<Serializable, Session>(name, new ConcurrentHashMap<Serializable, Session>());
+            }
+        });
     }
 
     @Override
     protected void doUpdate(Session session) {
-        super.doUpdate(session);
         BoundValueOperations<String, Object> sessionValueOperations = sessionRedisTemplate
                 .boundValueOps(sessionKeyGenerator(session.getId().toString()));
         sessionValueOperations.set(session);
@@ -33,12 +43,13 @@ public class RedisSessionDao extends EnterpriseCacheSessionDAO {
 
     @Override
     protected void doDelete(Session session) {
-       sessionRedisTemplate.delete(sessionKeyGenerator(session.getId().toString()));
+        sessionRedisTemplate.delete(sessionKeyGenerator(session.getId().toString()));
     }
 
     @Override
     protected Serializable doCreate(Session session) {
-        Serializable sessionId = super.doCreate(session);
+        Serializable sessionId = generateSessionId(session);
+        assignSessionId(session, sessionId);
         BoundValueOperations<String, Object> stringObjectBoundValueOperations = sessionRedisTemplate.
                 boundValueOps(sessionKeyGenerator(session.getId().toString()));
         stringObjectBoundValueOperations.set(session, TIMEOUT, TimeUnit.MINUTES);
@@ -47,12 +58,12 @@ public class RedisSessionDao extends EnterpriseCacheSessionDAO {
 
     @Override
     protected Session doReadSession(Serializable sessionId) {
-        Session session = super.doReadSession(sessionId);
-        if (session == null) {
-            String sessionKey = sessionKeyGenerator(sessionId.toString());
-            BoundValueOperations<String, Object> sessionValue = sessionRedisTemplate.boundValueOps(sessionKey);
-            session = (Session) sessionValue.get();
-        }
+//        Session session = super.doReadSession(sessionId);
+//        if (session == null) {
+        String sessionKey = sessionKeyGenerator(sessionId.toString());
+        BoundValueOperations<String, Object> sessionValue = sessionRedisTemplate.boundValueOps(sessionKey);
+        Session session = (Session) sessionValue.get();
+//        }
         return session;
     }
 
